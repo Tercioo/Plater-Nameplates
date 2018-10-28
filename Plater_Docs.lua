314668585c7f405a10a7330b77bbfdd359ff6f24
Tercio <Tercio@localhost>
2018-10-28 00:24:56 -0300

    - Added Shaman's Sundering to the crowd control spells. - Added new file for documentations.

---
+++ Plater_Docs.lua
@@ -0,0 +1,25 @@
+
+local _
+
+do 
+	
+	PlaterDocs = {}
+	
+	PlaterDocs.UnitFrame = {
+		
+		childs = {
+			
+		},
+		
+		members = {
+			
+		},
+		
+		info = {
+			desc = "parent of all components that make up the nameplate.",
+			parent = "plateFrame",
+			type = "button",
+		},
+	
+	}
+end
