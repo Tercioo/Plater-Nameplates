e67c67c5aab40360b4b0f352e11a3f4b69789920
Continuity <continuity@gmx.net>
2020-01-04 01:05:56 +0100

    - DF lib update for IconRow functionality

--- libs/DF/fw.lua
+++ libs/DF/fw.lua
@@ -1,5 +1,5 @@
 
-local dversion = 162
+local dversion = 163
 
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary (major, minor)
--- libs/DF/panel.lua
+++ libs/DF/panel.lua
@@ -5009,11 +5009,18 @@ DF.IconRowFunctions = {
 			cooldownFrame:SetFrameLevel (newIconFrame:GetFrameLevel()+1)
 			
 			newIconFrame.CountdownText = cooldownFrame:CreateFontString (nil, "overlay", "GameFontNormal")
-			newIconFrame.CountdownText:SetPoint ("center")
+			--newIconFrame.CountdownText:SetPoint ("center")
+			newIconFrame.CountdownText:SetPoint (self.options.text_anchor or "center", newIconFrame, self.options.text_rel_anchor or "center", self.options.text_x_offset or 0, self.options.text_y_offset or 0)
 			newIconFrame.CountdownText:Hide()
 			
+			newIconFrame.StackText = cooldownFrame:CreateFontString (nil, "overlay", "GameFontNormal")
+			--newIconFrame.StackText:SetPoint ("bottomright")
+			newIconFrame.StackText:SetPoint (self.options.stack_text_anchor or "center", newIconFrame, self.options.stack_text_rel_anchor or "bottomright", self.options.stack_text_x_offset or 0, self.options.stack_text_y_offset or 0)
+			newIconFrame.StackText:Hide()
+			
 			newIconFrame.Desc = newIconFrame:CreateFontString (nil, "overlay", "GameFontNormal")
-			newIconFrame.Desc:SetPoint ("bottom", newIconFrame, "top", 0, 2)
+			--newIconFrame.Desc:SetPoint ("bottom", newIconFrame, "top", 0, 2)
+			newIconFrame.Desc:SetPoint(self.options.desc_text_anchor or "bottom", newIconFrame, self.options.desc_text_rel_anchor or "top", self.options.desc_text_x_offset or 0, self.options.desc_text_y_offset or 2)
 			newIconFrame.Desc:Hide()
 			
 			newIconFrame.Cooldown = cooldownFrame
@@ -5051,7 +5058,7 @@ DF.IconRowFunctions = {
 		return iconFrame
 	end,
 	
-	SetIcon = function (self, spellId, borderColor, startTime, duration, forceTexture, descText)
+	SetIcon = function (self, spellId, borderColor, startTime, duration, forceTexture, descText, count, debuffType, caster, canStealOrPurge)
 	
 		local spellName, _, spellIcon
 	
@@ -5090,6 +5097,7 @@ DF.IconRowFunctions = {
 						formattedTime = floor (formattedTime)
 					end
 					
+					iconFrame.CountdownText:SetPoint (self.options.text_anchor or "center", iconFrame, self.options.text_rel_anchor or "center", self.options.text_x_offset or 0, self.options.text_y_offset or 0)
 					iconFrame.CountdownText:SetText (formattedTime)
 					iconFrame.Cooldown:SetHideCountdownNumbers (true)
 				else
@@ -5104,9 +5112,20 @@ DF.IconRowFunctions = {
 				iconFrame.Desc:Show()
 				iconFrame.Desc:SetText (descText.text)
 				iconFrame.Desc:SetTextColor (DF:ParseColors (descText.text_color or self.options.desc_text_color))
+				iconFrame.Desc:SetPoint(self.options.desc_text_anchor or "bottom", iconFrame, self.options.desc_text_rel_anchor or "top", self.options.desc_text_x_offset or 0, self.options.desc_text_y_offset or 2)
 				DF:SetFontSize (iconFrame.Desc, descText.text_size or self.options.desc_text_size)
 			else
 				iconFrame.Desc:Hide()
+			end
+			
+			if (count and count > 1 and self.options.stack_text) then
+				iconFrame.StackText:Show()
+				iconFrame.StackText:SetText (count)
+				iconFrame.StackText:SetTextColor (DF:ParseColors (self.options.desc_text_color))
+				iconFrame.StackText:SetPoint (self.options.stack_text_anchor or "center", iconFrame, self.options.stack_text_rel_anchor or "bottomright", self.options.stack_text_x_offset or 0, self.options.stack_text_y_offset or 0)
+				DF:SetFontSize (iconFrame.StackText, self.options.stack_text_size)
+			else
+				iconFrame.StackText:Hide()
 			end
 			
 			PixelUtil.SetSize (iconFrame, self.options.icon_width, self.options.icon_height)
@@ -5115,6 +5134,15 @@ DF.IconRowFunctions = {
 			--> update the size of the frame
 			self:SetWidth ((self.options.left_padding * 2) + (self.options.icon_padding * (self.NextIcon-2)) + (self.options.icon_width * (self.NextIcon - 1)))
 			self:SetHeight (self.options.icon_height + (self.options.top_padding * 2))
+
+			--> make information available
+			iconFrame.spellId = spellId
+			iconFrame.startTime = startTime
+			iconFrame.duration = duration
+			iconFrame.count = count
+			iconFrame.debuffType = debuffType
+			iconFrame.caster = caster
+			iconFrame.canStealOrPurge = canStealOrPurge
 
 			--> show the frame
 			self:Show()
@@ -5175,9 +5203,24 @@ local default_icon_row_options = {
 	texcoord = {.1, .9, .1, .9},
 	show_text = true,
 	text_color = {1, 1, 1, 1},
+	text_anchor = "center",
+	text_rel_anchor = "center",
+	text_x_offset = 0,
+	text_y_offset = 0,
 	desc_text = true,
 	desc_text_color = {1, 1, 1, 1},
 	desc_text_size = 7,
+	desc_text_anchor = "bottom",
+	desc_text_rel_anchor = "top",
+	desc_text_x_offset = 0,
+	desc_text_y_offset = 2,
+	stack_text = true,
+	stack_text_color = {1, 1, 1, 1},
+	stack_text_size = 10,
+	stack_text_anchor = "center",
+	stack_text_rel_anchor = "bottomright",
+	stack_text_x_offset = 0,
+	stack_text_y_offset = 0,
 	left_padding = 1, --distance between right and left
 	top_padding = 1, --distance between top and bottom 
 	icon_padding = 1, --distance between each icon
