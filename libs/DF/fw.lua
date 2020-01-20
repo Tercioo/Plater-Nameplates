6c75888fc528c88f171d8fc86d955cafcdd7c520
Continuity <continuity@gmx.net>
2020-01-20 21:49:09 +0100

    - adding cooldown text size setting for buff special

--- libs/DF/fw.lua
+++ libs/DF/fw.lua
@@ -1,5 +1,5 @@
 
-local dversion = 164
+local dversion = 165
 
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary (major, minor)
--- libs/DF/panel.lua
+++ libs/DF/panel.lua
@@ -5098,6 +5098,7 @@ DF.IconRowFunctions = {
 					end
 					
 					iconFrame.CountdownText:SetPoint (self.options.text_anchor or "center", iconFrame, self.options.text_rel_anchor or "center", self.options.text_x_offset or 0, self.options.text_y_offset or 0)
+					DF:SetFontSize (iconFrame.CountdownText, self.options.text_size)
 					iconFrame.CountdownText:SetText (formattedTime)
 					iconFrame.Cooldown:SetHideCountdownNumbers (true)
 				else
@@ -5203,6 +5204,7 @@ local default_icon_row_options = {
 	texcoord = {.1, .9, .1, .9},
 	show_text = true,
 	text_color = {1, 1, 1, 1},
+	text_size = 12,
 	text_anchor = "center",
 	text_rel_anchor = "center",
 	text_x_offset = 0,
