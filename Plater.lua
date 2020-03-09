dd2bff7d4369b8cd5dcb1e2bd72da94a99070e35
mariocki <mariocki@gmail.com>
2020-03-09 18:27:46 +0000

    Update Plater.lua
    Create two new API methods for use in Modding:
    
    Plater.SetNameplateFontOutline
    Plater.ResetNameplateFontOutline

--- Plater.lua
+++ Plater.lua
@@ -9074,6 +9074,32 @@ end
 		end
 	end
 
+	local function inTable(tbl, item)
+		for key, value in pairs(tbl) do
+			if value == item then return key end
+		end
+		return false
+	end
+
+	function Plater.SetNameplateFontOutline (unitFrame, outline)
+		if (unitFrame.unit) then
+			local outline_modes = {"NONE", "MONOCHROME", "OUTLINE", "THICKOUTLINE"}
+			local plateConfigs = DB_PLATE_CONFIG[unitFrame.ActorType]
+			if (outline and outline ~= '' and inTable(outline_modes, outline)) then
+				Plater.SetFontOutlineAndShadow (unitFrame.unitName, outline, plateConfigs.actorname_text_shadow_color, plateConfigs.actorname_text_shadow_color_offset[1], plateConfigs.actorname_text_shadow_color_offset[2])
+			else
+				Plater.SetFontOutlineAndShadow (unitFrame.unitName, plateConfigs.actorname_text_outline, plateConfigs.actorname_text_shadow_color, plateConfigs.actorname_text_shadow_color_offset[1], plateConfigs.actorname_text_shadow_color_offset[2])
+			end
+		end
+	end
+
+	function Plater.ResetNameplateFontOutline (unitFrame)
+		if (unitFrame.unit) then
+			local plateConfigs = DB_PLATE_CONFIG[unitFrame.ActorType]
+			Plater.SetFontOutlineAndShadow (unitFrame.unitName, plateConfigs.actorname_text_outline, plateConfigs.actorname_text_shadow_color, plateConfigs.actorname_text_shadow_color_offset[1], plateConfigs.actorname_text_shadow_color_offset[2])
+		end
+	end
+
 	--set a temporarly size for the healthbar
 	--this value is reset when the nameplate is added to the screen
 	function Plater.SetNameplateSize (unitFrame, width, height)
