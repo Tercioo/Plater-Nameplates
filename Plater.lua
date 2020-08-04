15e3fbd5619c683127f7524d8e0928a43da739de
Continuity <continuity@gmx.net>
2020-08-04 16:10:29 +0200

    - adding "IsFocus" cache

--- Plater.lua
+++ Plater.lua
@@ -6212,14 +6212,18 @@ end
 	-- ~target
 	function Plater.UpdateTarget (plateFrame) --private
 
-		if (UnitIsUnit (plateFrame.unitFrame [MEMBER_UNITID], "focus") and Plater.db.profile.focus_indicator_enabled) then
-			--this is a rare call, no need to cache these values
-			local texture = LibSharedMedia:Fetch ("statusbar", Plater.db.profile.focus_texture)
-			plateFrame.FocusIndicator:SetTexture (texture)
-			plateFrame.FocusIndicator:SetVertexColor (unpack (Plater.db.profile.focus_color))
-			plateFrame.FocusIndicator:Show()
+		if UnitIsUnit (plateFrame.unitFrame [MEMBER_UNITID], "focus") then
+			if Plater.db.profile.focus_indicator_enabled then
+				--this is a rare call, no need to cache these values
+				local texture = LibSharedMedia:Fetch ("statusbar", Plater.db.profile.focus_texture)
+				plateFrame.FocusIndicator:SetTexture (texture)
+				plateFrame.FocusIndicator:SetVertexColor (unpack (Plater.db.profile.focus_color))
+				plateFrame.FocusIndicator:Show()
+			end
+			plateFrame.unitFrame.IsFocus = true
 		else
 			plateFrame.FocusIndicator:Hide()
+			plateFrame.unitFrame.IsFocus = false
 		end
 		
 		plateFrame.unitFrame.WidgetContainer:UnregisterForWidgetSet()
