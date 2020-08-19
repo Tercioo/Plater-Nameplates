b945c2f27a761483f0aff5df47e54908e81b5117
Continuity <continuity@gmx.net>
2020-08-19 21:24:39 +0200

    Merge remote-tracking branch 'upstream/master'

--- Plater.lua
+++ Plater.lua
@@ -8435,11 +8435,12 @@ end
 	end
 	
 	function Plater.GetUnitType (plateFrame)
-		if (PET_CACHE [plateFrame [MEMBER_GUID]]) then
+		if (plateFrame ["namePlateClassification"] == "minus") then
+			return "minus"
+			
+		elseif (PET_CACHE [plateFrame [MEMBER_GUID]]) then
 			return "pet"
 			
-		elseif (plateFrame ["namePlateClassification"] == "minus") then
-			return "minus"
 		end
 		
 		return "normal"
