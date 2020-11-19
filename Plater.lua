cd49c4a2fff078589ce0fa7da37d6dd5cb4c991a
Continuity <continuity@gmx.net>
2020-11-19 12:07:24 +0100

    - updating profiling for hooks/scripts

--- Plater.lua
+++ Plater.lua
@@ -8838,9 +8838,12 @@ end
 				scriptInfo.GlobalScriptObject.NeedHotReload = false
 
 				if (globalScriptObject.HasConstructor and (not scriptInfo.Initialized or (isHookScript and forceHotReload))) then
+					local modName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+					Plater.StartLogPerformance("Mod-Constructor", modName)
 					local okay, errortext = pcall (globalScriptObject.Constructor, self, self.displayedUnit or self.unit or self:GetParent()[MEMBER_UNITID], self, scriptInfo.Env, PLATER_GLOBAL_MOD_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
+					Plater.EndLogPerformance("Mod-Constructor", modName)
 					if (not okay) then
-						Plater:Msg ("Mod |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r Constructor error: " .. errortext)
+						Plater:Msg ("Mod |cFFAAAA22" .. modName .. "|r Constructor error: " .. errortext)
 					end
 					scriptInfo.Initialized = true
 				end
@@ -8860,7 +8863,10 @@ end
 
 				--dispatch the constructor
 				local unitFrame = self.unitFrame or self
+				local scriptName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+				Plater.StartLogPerformance("Script-Constructor", scriptName)
 				local okay, errortext = pcall (scriptInfo.GlobalScriptObject ["ConstructorCode"], self, unitFrame.displayedUnit or unitFrame.unit or unitFrame.PlateFrame[MEMBER_UNITID], unitFrame, scriptInfo.Env, PLATER_GLOBAL_SCRIPT_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
+				Plater.EndLogPerformance("Script-Constructor", scriptName)
 				if (not okay) then
 					Plater:Msg ("Script |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r Constructor error: " .. errortext)
 				end
@@ -8878,9 +8884,12 @@ end
 			
 			--dispatch the runtime script
 			local unitFrame = self.unitFrame or self
+			local scriptName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+			Plater.StartLogPerformance("Script-OnUpdate", scriptName)
 			local okay, errortext = pcall (scriptInfo.GlobalScriptObject ["UpdateCode"], self, unitFrame.displayedUnit or unitFrame.unit or unitFrame.PlateFrame[MEMBER_UNITID], unitFrame, scriptInfo.Env, PLATER_GLOBAL_SCRIPT_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
+			Plater.EndLogPerformance("Script-OnUpdate", scriptName)
 			if (not okay) then
-				Plater:Msg ("Script |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r OnUpdate error: " .. errortext)
+				Plater:Msg ("Script |cFFAAAA22" .. scriptName.. "|r OnUpdate error: " .. errortext)
 			end
 		end,
 		
@@ -8890,9 +8899,12 @@ end
 			local unitFrame = self.unitFrame or self
 			scriptInfo.Env._DefaultWidth = self:GetWidth()
 			scriptInfo.Env._DefaultHeight = self:GetHeight()
+			local scriptName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+			Plater.StartLogPerformance("Script-OnShow", scriptName)
 			local okay, errortext = pcall (scriptInfo.GlobalScriptObject ["OnShowCode"], self, unitFrame.displayedUnit or unitFrame.unit or unitFrame.PlateFrame[MEMBER_UNITID], unitFrame, scriptInfo.Env, PLATER_GLOBAL_SCRIPT_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
+			Plater.EndLogPerformance("Script-OnShow", scriptName)
 			if (not okay) then
-				Plater:Msg ("Script |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r OnShow error: " .. errortext)
+				Plater:Msg ("Script |cFFAAAA22" .. scriptName .. "|r OnShow error: " .. errortext)
 			end
 			
 			scriptInfo.IsActive = true
@@ -8903,9 +8915,12 @@ end
 		ScriptRunOnHide = function (self, scriptInfo)
 			--dispatch the on hide script
 			local unitFrame = self.unitFrame or self
+			local scriptName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+			Plater.StartLogPerformance("Script-OnHide", scriptName)
 			local okay, errortext = pcall (scriptInfo.GlobalScriptObject ["OnHideCode"], self, unitFrame.displayedUnit or unitFrame.unit or unitFrame.PlateFrame[MEMBER_UNITID], unitFrame, scriptInfo.Env, PLATER_GLOBAL_SCRIPT_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
+			Plater.EndLogPerformance("Script-OnHide", scriptName)
 			if (not okay) then
-				Plater:Msg ("Script |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r OnHide error: " .. errortext)
+				Plater:Msg ("Script |cFFAAAA22" .. scriptName .. "|r OnHide error: " .. errortext)
 			end
 			
 			scriptInfo.IsActive = false
@@ -8915,29 +8930,36 @@ end
 		--run the Initialization script, called during compile time
 		ScriptRunInitialization = function (globalScriptObject)
 			--dispatch the init script
+			local scriptName = globalScriptObject.DBScriptObject.Name
+			Plater.StartLogPerformance("Script-OnHide", scriptName)
 			local okay, errortext = pcall (globalScriptObject ["Initialization"], PLATER_GLOBAL_SCRIPT_ENV [globalScriptObject.DBScriptObject.scriptId])
+			Plater.EndLogPerformance("Script-Initialization", scriptName)
 			if (not okay) then
-				Plater:Msg ("Script |cFFAAAA22" .. globalScriptObject.DBScriptObject.Name .. "|r Initialization error: " .. errortext)
+				Plater:Msg ("Script |cFFAAAA22" .. scriptName .. "|r Initialization error: " .. errortext)
 			end
 		end,
 		
 		ScriptRunHook = function (self, scriptInfo, hookName, frame)
 			--dispatch a hook for the script
 			--at the moment, self is always the unit frame
-			Plater.StartLogPerformance("hooks", scriptInfo.GlobalScriptObject.DBScriptObject.Name)
+			local modName = scriptInfo.GlobalScriptObject.DBScriptObject.Name
+			Plater.StartLogPerformance("Mod-RunHooks-" .. hookName, modName)
 			local okay, errortext = pcall (scriptInfo.GlobalScriptObject [hookName], frame or self, self.displayedUnit, self, scriptInfo.Env, PLATER_GLOBAL_MOD_ENV [scriptInfo.GlobalScriptObject.DBScriptObject.scriptId])
-			Plater.EndLogPerformance("hooks", scriptInfo.GlobalScriptObject.DBScriptObject.Name)
+			Plater.EndLogPerformance("Mod-RunHooks-" .. hookName, modName)
 			if (not okay) then
-				Plater:Msg ("Mod |cFFAAAA22" .. scriptInfo.GlobalScriptObject.DBScriptObject.Name .. "|r code for |cFFBB8800" .. hookName .. "|r error: " .. errortext)
+				Plater:Msg ("Mod |cFFAAAA22" .. modName .. "|r code for |cFFBB8800" .. hookName .. "|r error: " .. errortext)
 			end
 		end,
 		
 		--run only once without attach to the script or hook
 		ScriptRunNoAttach = function (hookInfo, hookName)
 			local func = hookInfo [hookName]
+			local modName = hookInfo.DBScriptObject.Name
+			Plater.StartLogPerformance("Mod-RunHooks-NoAttach-" .. hookName, modName)
 			local okay, errortext = pcall (func, PLATER_GLOBAL_MOD_ENV [hookInfo.DBScriptObject.scriptId])
+			Plater.EndLogPerformance("Mod-RunHooks-NoAttach-" .. hookName, modName)
 			if (not okay) then
-				Plater:Msg ("Mod |cFFAAAA22" .. hookInfo.DBScriptObject.Name .. "|r code for |cFFBB8800" .. hookName .. "|r error: " .. errortext)
+				Plater:Msg ("Mod |cFFAAAA22" .. modName .. "|r code for |cFFBB8800" .. hookName .. "|r error: " .. errortext)
 			end
 		end,
 		
@@ -10798,7 +10820,7 @@ function SlashCmdList.PLATER (msg, editbox)
 		return
 	
 	elseif (msg == "profprint") then
-		Plater.DumpPerformance()
+		Plater.DumpPerformance(true)
 		
 		return
 	
--- Plater_Profiling.lua
+++ Plater_Profiling.lua
@@ -1,16 +1,39 @@
 -- profiling support (WIP)
 
-local profData
+local profData = {}
 local profilingEnabled = false
 
+-- helper
+local function round(x)
+	if not x then return nil end
+	return (x + 0.5 - (x + 0.5) % 1)
+end
+
+local function roundTime(value)
+	if not value then return nil end
+	return round(value*100000)/100000
+end
+
+local function roundPercent(value)
+	if not value then return nil end
+	return round(value*10000)/10000
+end
+
+
+-- profiling
 function Plater.EnableProfiling()
 	profilingEnabled = true
 	
 	profData = {}
+	profData.startTime = debugprofilestop()
+	profData.endTime = nil
+	profData.data = {}
 end
 
 function Plater.DisableProfiling()
 	profilingEnabled = false
+	
+	profData.endTime = debugprofilestop()
 	
 end
 
@@ -18,37 +41,71 @@ end
 function Plater.StartLogPerformance(pType, name)
 	if not profilingEnabled or not pType or not name then return end
 	
-	if not profData[pType] then
-		profData[pType] = {}
+	if not profData.data[pType] then
+		profData.data[pType] = {}
 	end
-	if not profData[pType][name] then
-		profData[pType][name] = {}
+	if not profData.data[pType][name] then
+		profData.data[pType][name] = {}
 	end
 	
-	profData[pType][name].curStartTime = debugprofilestop()
+	profData.data[pType][name].curStartTime = debugprofilestop()
 end
 
 --pType = profiling type (e.g. hooks)
 function Plater.EndLogPerformance(pType, name)
 	if not profilingEnabled or not pType or not name then return end
 	
-	profData[pType][name].totalTime = (profData[pType][name].totalTime or 0) + (debugprofilestop() - profData[pType][name].curStartTime)
-	profData[pType][name].count = (profData[pType][name].count or 0) + 1
-	profData[pType][name].curStartTime = nil
+	local data = profData.data[pType][name]
+	data.totalTime = (data.totalTime or 0) + (debugprofilestop() - data.curStartTime)
+	data.count = (data.count or 0) + 1
+	data.curStartTime = nil
 end
 
-function Plater.DumpPerformance()
+function Plater.DumpPerformance(printOut)
 	local perf = {}
-	for pType, data in pairs(profData) do
-		print(pType .. ":")
+	local printStr = "Plater profiling data:" .. "\n"
+	
+	perf.totalGlobalTime = (profData.endTime or debugprofilestop()) - (profData.startTime or debugprofilestop())
+	
+	local sumTimePTypes = 0
+	local sumExecPTypes = 0
+	for pType, data in pairs(profData.data) do
 		perf[pType] = {}
+		local pTypeTime = 0
+		local pTypeExec = 0
+		
+		printStr = printStr .. pType .. ":" .. "\n"
 		for name, pData in pairs(data) do
-			perf[pType][name] = "avg: " .. (pData.totalTime / pData.count) .. " - count: " .. pData.count .. " - total: " .. pData.totalTime
-			print("  " .. name .. " - " .. perf[pType][name])
+			perf[pType][name] = "avg: " .. roundTime(pData.totalTime / pData.count) .. "ms - count: " .. pData.count .. " - total: " .. roundTime(pData.totalTime) .. "ms"
+			pTypeTime = pTypeTime + pData.totalTime
+			pTypeExec = pTypeExec + pData.count
+			printStr = printStr .. "  " .. name .. " - " .. perf[pType][name] .. "\n"
 		end
+		perf[pType].pTypeTime = pTypeTime
+		perf[pType].pTypeExec = pTypeExec
+		perf[pType].pTypeGlobalPercent = pTypeTime / perf.totalGlobalTime
+		printStr = printStr .. "-> count: " .. pTypeExec .. " - time: "  .. roundTime(pTypeTime) .. "ms - %global: " .. roundPercent(perf[pType].pTypeGlobalPercent) .. "%" .. "\n"
+		sumTimePTypes = sumTimePTypes + pTypeTime
+		sumExecPTypes = sumExecPTypes + pTypeExec
 	end
+	
+	perf.timeInPlaterProfile = sumTimePTypes
+	perf.totalLoggedEvents = sumExecPTypes
+	perf.totalAveragePerEvent = sumTimePTypes / sumExecPTypes
+	perf.percentGlobalInPlater = sumTimePTypes / perf.totalGlobalTime
+	printStr = printStr .. "Plater profiling totals:"
+	printStr = printStr .. "  Profiling time: " .. roundTime(perf.totalGlobalTime / 100000)*100 .. "s" .. "\n"
+	printStr = printStr .. "  Time in Plater: " .. roundTime(perf.timeInPlaterProfile) .. "ms" .. "\n"
+	printStr = printStr .. "  Logged events: " .. perf.totalLoggedEvents .. "\n"
+	printStr = printStr .. "  Average runtimetime of event: " .. roundTime(perf.totalAveragePerEvent) .. "ms" .. "\n"
+	printStr = printStr .. "  % of global time: " .. roundPercent(perf.percentGlobalInPlater) .. "%" .. "\n"
+	
 	if ViragDevTool_AddData then
 		ViragDevTool_AddData(perf,"Plater Profiling")
 	end
+	if printOut then
+		print(printStr)
+	end
+	
 	return perf
 end
