d45a12eba4261c8e826d332a3ad2ab1f8d72e526
Continuity <continuity@gmx.net>
2021-01-29 11:08:26 +0100

    - only show debug clickspace if nameplate is visible

--- Plater.lua
+++ Plater.lua
@@ -5187,7 +5187,7 @@ end
 		
 		if (isDebug and not Plater.db.profile.click_space_always_show) then
 			for _, plateFrame in ipairs (Plater.GetAllShownPlates()) do
-				if not plateFrame.IsSelf then
+				if not plateFrame.IsSelf and plateFrame.unitFrame.PlaterOnScreen then
 					Plater.ShowClickSpace (plateFrame)
 					
 					if (Plater.PlateSizeDebugTimer and not Plater.PlateSizeDebugTimer._cancelled) then
