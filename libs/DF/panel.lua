bea18704647c3bccb2345635491e50410b9861b7
Continuity <continuity@gmx.net>
2021-03-23 20:08:52 +0100

    - DF lib update

--- libs/DF/panel.lua
+++ libs/DF/panel.lua
@@ -9194,7 +9194,7 @@ end
 	}
 
 -- ~unitframe
-local globalBaseFrameLevel = 0 -- to be increased + used across each new plate
+local globalBaseFrameLevel = 1 -- to be increased + used across each new plate
 function DF:CreateUnitFrame (parent, name, unitFrameSettingsOverride, healthBarSettingsOverride, castBarSettingsOverride, powerBarSettingsOverride)
 	
 	local parentName = name or ("DetailsFrameworkUnitFrame" .. tostring (math.random (1, 100000000)))
@@ -9205,7 +9205,9 @@ function DF:CreateUnitFrame (parent, name, unitFrameSettingsOverride, healthBarS
 	--> base level
 	--local baseFrameLevel = f:GetFrameLevel()
 	local baseFrameLevel = globalBaseFrameLevel
-	globalBaseFrameLevel = globalBaseFrameLevel + 10
+	globalBaseFrameLevel = globalBaseFrameLevel + 50
+	
+	f:SetFrameLevel (baseFrameLevel)
 	
 	--> create the healthBar
 	local healthBar = DF:CreateHealthBar (f, false, healthBarSettingsOverride)
