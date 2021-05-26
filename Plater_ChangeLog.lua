febca008c90c76699e22c5483a9debc71c8477b0
Continuity <continuity@gmx.net>
2021-05-26 12:28:29 +0200

    - update changelog

--- Plater_ChangeLog.lua
+++ Plater_ChangeLog.lua
@@ -9,6 +9,7 @@ function Plater.GetChangelogTable()
 		Plater.ChangeLogTable = {
 		
 			--{1621935367,  "New Feature", "May 25th, 2021", "Mod 'Comm Message' hook to react to 'Plater.SendComm(...)' calls from the exact same mod of another user in party or raid.", "Terciob"},
+			{1621935367,  "Backend Change", "May 25th, 2021", "Mod hook 'Player Power Update' now provides the energy type as additional payload in the vararg parameter.", "cont1nuity"},
 			{1621935367,  "Bug Fix", "May 25th, 2021", "Alpha of non-targets should now work correctly again.", "cont1nuity"},
 			{1621935367,  "Backend Change", "May 25th, 2021", "Enabling execute range indicator for warriors in TBC.", "cont1nuity"},
 			{1621935367,  "Bug Fix", "May 25th, 2021", "'Combo Points' mod now handles targeted combo points correctly in TBC.", "cont1nuity"},
