d9230f712e5d09572cc8044a2b68f5be655c4e93
Tercio Jose <terciob19@hotmail.com>
2021-07-09 11:34:47 -0300

    DB_CAPTURED_CASTS won't store casts started by players

--- Plater.lua
+++ Plater.lua
@@ -8281,12 +8281,11 @@ end
 		SPELL_CAST_START = function (time, token, hidding, sourceGUID, sourceName, sourceFlag, sourceFlag2, targetGUID, targetName, targetFlag, targetFlag2, spellID, spellName, spellType, amount, overKill, school, resisted, blocked, absorbed, isCritical)
 			-- SPELL_CAST_START does not fire for instant casts, no need to capture those
 			if (not DB_CAPTURED_CASTS[spellID]) then
-
 				--check if this is a player spell
 				if (sourceFlag and bit.band(sourceFlag, 0x00000400) ~= 0) then --0x00000400 = player flag
-					local _, unitClass = UnitClass(sourceName)
-					DB_CAPTURED_CASTS[spellID] = {npcID = Plater:GetNpcIdFromGuid(sourceGUID or ""), encounterID = "", encounterName = unitClass or ""}
-
+					--ignore player spells
+					--local _, unitClass = UnitClass(sourceName)
+					--DB_CAPTURED_CASTS[spellID] = {npcID = Plater:GetNpcIdFromGuid(sourceGUID or ""), encounterID = "", encounterName = unitClass or ""}
 				else
 					DB_CAPTURED_CASTS[spellID] = {npcID = Plater:GetNpcIdFromGuid(sourceGUID or ""), encounterID = Plater.CurrentEncounterID, encounterName = Plater.CurrentEncounterName}
 				end
