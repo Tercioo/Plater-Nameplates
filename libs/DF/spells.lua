c3c7fa09b9395a96fb8a26c0e3fd18507a650fcd
Casey Raethke <7716908+raethkcj@users.noreply.github.com>
2022-03-20 17:45:48 -0500

    Disable Vampiric Embrace as a player cooldown in Classic

--- libs/DF/spells.lua
+++ libs/DF/spells.lua
@@ -623,6 +623,7 @@ if (IS_WOW_PROJECT_NOT_MAINLINE) then
 	DF.CooldownsBySpec[258][19242] = 5 --desperate prayer Rank 6
 	DF.CooldownsBySpec[258][19243] = 5 --desperate prayer Rank 7
 	DF.CooldownsBySpec[258][25437] = 5 --desperate prayer Rank 8
+	DF.CooldownsBySpec[258][15286] = nil --vampiric embrace is a debuff on target in classic, not a buff on priest
 
 	--ROGUE - 259
 	DF.CooldownsBySpec[259][1857] = 2 --vanish Rank 2
