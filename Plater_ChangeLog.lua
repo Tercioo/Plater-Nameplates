6478b46fba8dc509bb109d7a14f58e7f37894cc0
Tercio Jose <terciob19@hotmail.com>
2022-12-27 10:54:40 -0300

    Change logs

--- Plater_ChangeLog.lua
+++ Plater_ChangeLog.lua
@@ -7,7 +7,8 @@ local _
 function Plater.GetChangelogTable()
 	if (not Plater.ChangeLogTable) then
 		Plater.ChangeLogTable = {
-		
+			{1672013181,  "New Feature", "December 27th, 2022", "Added 'Send to Raid' button on Npc and Cast customization tab.", "Terciob"},
+
 			{1672013181,  "Bug Fix", "December 26th, 2022", "Fixing charged Combo Points not updating properly in some cases.", "cont1nuity"},
 			{1672013181,  "Bug Fix", "December 26th, 2022", "Fixing cast color/sounds not working after profile import.", "cont1nuity"},
 			
