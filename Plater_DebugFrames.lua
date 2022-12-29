d3689ea12f1fdd8c69756f5a67a90a13a42ef8bc
Tercio Jose <terciob19@hotmail.com>
2022-12-28 22:16:17 -0300

    Added a function call to open Frame Inspect to inspect a nameplate

---
+++ Plater_DebugFrames.lua
@@ -0,0 +1,19 @@
+
+local Plater = _G.Plater
+local C_Timer = _G.C_Timer
+local addonName, platerInternal = ...
+local DF = DetailsFramework
+
+function Plater.DebugTargetNameplate()
+    local plateFrame = C_NamePlate.GetNamePlateForUnit("target", issecure())
+    if (plateFrame) then
+        if (not _G.FrameInspect) then
+            Plater:Msg("Please install FrameInspect addon to use this function.")
+            return
+        end
+        _G.FrameInspect.Inspect(plateFrame)
+    else
+        Plater:Msg("You don't have a target or the nameplate is protected.")
+            return
+    end
+end
