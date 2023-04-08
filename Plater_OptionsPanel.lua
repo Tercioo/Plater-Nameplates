bc1a792a7475567440b4d6d90be292291801e884
Continuity <continuity@gmx.net>
2023-04-08 19:56:37 +0200

    - fixing outline options

--- Plater_OptionsPanel.lua
+++ Plater_OptionsPanel.lua
@@ -1158,8 +1158,8 @@ function Plater.OpenOptionsPanel()
 	local textures = LibSharedMedia:HashTable ("statusbar")
 
 	--outline table
-	local outline_modes = {"NONE", "MONOCHROME", "OPTIONS_OUTLINE", "THICKOUTLINE", "MONOCHROME, OUTLINE", "MONOCHROME, THICKOUTLINE"}
-	local outline_modes_names = {"None", "Monochrome", "OPTIONS_OUTLINE", "Thick Outline", "Monochrome Outline", "Monochrome Thick Outline"}
+	local outline_modes = {"NONE", "MONOCHROME", "OUTLINE", "THICKOUTLINE", "MONOCHROME, OUTLINE", "MONOCHROME, THICKOUTLINE"}
+	local outline_modes_names = {"None", "Monochrome", "Outline", "Thick Outline", "Monochrome Outline", "Monochrome Thick Outline"}
 	local build_outline_modes_table = function (actorType, member)
 		local t = {}
 		for i = 1, #outline_modes do
