46e8430ae01a375f7eee671cad00308ddc50e451
Continuity <continuity@gmx.net>
2023-05-28 22:23:05 +0200

    - more force in combat

--- Plater.lua
+++ Plater.lua
@@ -1280,6 +1280,8 @@ Plater.AnchorNamesByPhraseId = {
 		[202824] = true, --Erratic Remnant
 		[203230] = true, --Dragonfire Golem
 		[203812] = true, --Voice From Beyond
+		[98081] = true, -- Bellowing Idol
+		[92538] = true, -- Tarspitter Grub
 	}
 
 	--update the settings cache for scritps
