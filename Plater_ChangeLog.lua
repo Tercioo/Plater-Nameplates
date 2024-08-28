cfce7aed5b866b4512ac482f23a88aed549b87cb
Continuity <continuity@gmx.net>
2024-08-29 00:39:41 +0200

    - changelog

--- Plater_ChangeLog.lua
+++ Plater_ChangeLog.lua
@@ -8,6 +8,8 @@ function Plater.GetChangelogTable()
 	if (not Plater.ChangeLogTable) then
 		Plater.ChangeLogTable = {
 
+			{1724783240, "Bug Fix", "Aug 29th, 2024", "Fixing error in default script.", "Terciob"},
+			{1724783240, "Bug Fix", "Aug 28th, 2024", "Allowing resource scaling to be smaller.", "cont1nuity"},
 			{1724783240, "Bug Fix", "Aug 27th, 2024", "Fixing rogue combo points layout.", "cont1nuity"},
 			{1724783240, "New Feature", "Aug 27th, 2024", "Adding option to rename casts based on BigWigs renamed spells.", "cont1nuity"},
 			
