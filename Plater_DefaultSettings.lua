2f06d46f29f60919a6364f9a58798e4b8de70e91
Continuity <continuity@gmx.net>
2024-11-01 20:55:15 +0100

    - enable DBM support by default

--- Plater_DefaultSettings.lua
+++ Plater_DefaultSettings.lua
@@ -948,7 +948,7 @@ PLATER_DEFAULT_SETTINGS = {
 		
 		bossmod_support_enabled = true,
 		bossmod_castrename_enabled = true,
-		bossmod_support_bars_enabled = false,
+		bossmod_support_bars_enabled = true,
 		bossmod_support_bars_text_enabled = true,
 		bossmod_aura_height = 32,
 		bossmod_aura_width = 32,
