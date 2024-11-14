1bffeb0a74032ca0a5eea4d19c6f46382b47b278
Lynn <kjarli@gmail.com>
2024-11-15 00:55:03 +0100

    Added missing references to castBar.InterruptSourceName and castBar.InterruptSourceGUID in scripting panels (#250)

--- Plater_ChangeLog.lua
+++ Plater_ChangeLog.lua
@@ -8,6 +8,7 @@ function Plater.GetChangelogTable()
 	if (not Plater.ChangeLogTable) then
 		Plater.ChangeLogTable = {
 
+			{1729585511, "Bug Fix", "Nov 15th, 2024", "Added missing references to castBar.InterruptSourceName and castBar.InterruptSourceGUID in scripting panels.", "Linaori"},
 			{1729585511, "Backend Change", "Nov 14th, 2024", "Changed default values for DBM boss mod support and enabled it by default.", "cont1nuity"},
 			{1729585511, "New Feature", "Nov 14th, 2024", "Added glow options for expiring DBM boss mod icons.", "cont1nuity"},
 			{1729585511, "New Feature", "Nov 7th, 2024", "Added castBar.SpellNameRenamed, which contains either the customized or boss mods spell rename.", "Linaori"},
