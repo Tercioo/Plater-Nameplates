905f3ccf6c2bb94b3a85f61f0fed229ab3a26556
Tercio Jose <terciob19@hotmail.com>
2025-03-01 14:15:52 -0300

    Framework update

--- libs/DF/button.lua
+++ libs/DF/button.lua
@@ -897,8 +897,13 @@ end
 		self:SetScript("OnEnable", onEnableFunc)
 	end
 
+	---@class df_blizzbutton : button
+	---@field text fontstring
+	---@field MyObject df_button
+
 	---@class df_button : button, df_scripthookmixin, df_widgets
-	---@field widget button
+	---@field widget df_blizzbutton
+	---@field button df_blizzbutton
 	---@field tooltip string
 	---@field shown boolean
 	---@field width number
@@ -1009,9 +1014,7 @@ end
 		buttonObject.text_overlay = _G[name .. "_Text"]
 		buttonObject.disabled_overlay = _G[name .. "_TextureDisabled"]
 
-		-- "dungeons/textures/common/transparent.blp", an epmty texture as default.
-		-- emptystring seems to cause weird issues (GREEEEEEN!) under unknown circumstances
-		texture = texture ~= "" and texture or 982414
+		texture = texture or ""
 		
 		--check for atlas
 		local bSetTexture = false
@@ -1032,6 +1035,9 @@ end
 				local r, g, b, a = detailsFramework:ParseColors(texture)
 				self.icon:SetColorTexture(r, g, b, a)
 				bSetTexture = true
+
+			elseif (texture == "") then
+				bSetTexture = true -- setting textures with an empty string causes green rectangles
 			end
 		end
 
