f666675c450d6875513636f66a03833efcdd3426
cont1nuity <continuity@gmx.net>
2025-06-18 09:42:48 +0200

    - update framework

--- libs/DF/fw.lua
+++ libs/DF/fw.lua
@@ -1,6 +1,6 @@
 
 
-local dversion = 609
+local dversion = 611
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary(major, minor)
 
@@ -804,7 +804,8 @@ function DF.table.setfrompath(t, path, value)
 		local lastTable
 		local lastKey
 
-		for key in path:gmatch("[%w_]+") do
+		--for key in path:gmatch("[%w_]+") do
+		for key in path:gmatch("[^%.%[%]]+") do
 			lastTable = t
 			lastKey = key
 
@@ -4096,13 +4097,8 @@ function DF:CreateGlowOverlay(parent, antsColor, glowColor)
 	end
 
 	local glowFrame
-	if (buildInfo >= 110107) then --24-05-2025: in the 11.1.7 patch, the template used here does not exist anymore
-		--possible candidates to replace the template. template name and the parent key to the animation group:
-		--"ActionBarButtonAssistedCombatRotationTemplate".ActiveFrame.GlowAnim
-		--"ActionBarButtonAssistedCombatHighlightTemplate".Anim
-		--"ActionButtonTargetReticleFrameTemplate".HighlightAnim
-		--"ActionButtonTemplate".SpellHighlightAnim
-		glowFrame = CreateFrame("frame", frameName, parent)
+	if (buildInfo >= 110107) then --24-05-2025: in the 11.1.7 patch, the template used here does not exist anymore, replacement used
+		glowFrame = CreateFrame("frame", frameName, parent, "ActionButtonSpellAlertTemplate")
 	else
 		glowFrame = CreateFrame("frame", frameName, parent, "ActionBarButtonSpellActivationAlert")
 	end
