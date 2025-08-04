8e83ff1b13d18671f95db665bf8c8bcdb57db7fa
Lynn <kjarli@gmail.com>
2025-08-04 22:52:08 +0200

    Updated the wording of the priority tooltip (#254)
    * Updated the wording of the priority tooltip
    
    * Keep old and add new.
    
    ---------
    
    Co-authored-by: cont1nuity <continuity@gmx.net>

--- Plater_ScriptingPanels.lua
+++ Plater_ScriptingPanels.lua
@@ -1564,7 +1564,7 @@ end
 		local script_prio_label = DF:CreateLabel (parent, "Priority:", DF:GetTemplate ("font", "ORANGE_FONT_TEMPLATE"))
 		local script_prio_entry = DF:CreateSlider (parent, 191, 20, 1, 99, 1, 99, false, "ScriptPrioritySlider", _, _, options_slider_template, _)
 		script_prio_entry:SetPoint ("topleft", script_prio_label, "bottomleft", 0, -2)
-		script_prio_entry.tooltip = "Lower number -> higher priority.\nRight Click to Type the Value"
+		script_prio_entry.tooltip = "Lower number -> higher priority.\nHigher priority runs before lower priority.\nRight Click to Type the Value"
 		mainFrame.ScriptPrioSlideEntry = script_prio_entry
 
 		--options button
@@ -4821,3 +4821,4 @@ function Plater.CreateScriptingPanel()
 end
 
 --endd ends
+
