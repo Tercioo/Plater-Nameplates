fbafcf7fddd7446504c78960512161f1a476cbaf
cont1nuity <continuity@gmx.net>
2026-04-22 02:26:17 +0200

    - update lib DF

--- libs/DF/fw.lua
+++ libs/DF/fw.lua
@@ -1,5 +1,5 @@
 
-local dversion = 713
+local dversion = 714
 local major, minor = "DetailsFramework-1.0", dversion
 local DF, oldminor = LibStub:NewLibrary(major, minor)
 
@@ -1451,7 +1451,7 @@ function DF:SetFontFace(fontString, fontface)
 	end
 
 	local origFont, size, flags = fontString:GetFont()
-	pcall(fontString.SetFont, fontface, size, flags) -- silently fail this one
+	pcall(fontString.SetFont, fontString, fontface, size, flags) -- silently fail this one
 end
 
 local dummyFontString = UIParent:CreateFontString(nil, "background", "GameFontNormal")
